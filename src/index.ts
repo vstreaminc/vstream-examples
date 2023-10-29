d9e47e46d9baf25c18c314cd285c56d1e2f568b2
Stephen B King <stephen@vstream-corp.com>
2023-10-28 18:18:18 -0600

    feat: Create Commander program

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("VStream CLI");
+import { Command } from "@commander-js/extra-typings";
+
+const program = new Command();
+
+program
+  .name("vstream")
+  .description("VStream's official CLI app")
+  .version("0.0.1");
+
+program.parse();
